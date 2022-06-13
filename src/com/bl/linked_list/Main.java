f647cabbeec13ed2ccae3e87f849a8815fa6ba0d
Anicod <animeshraj735@gmail.com>
2022-06-13 10:14:31 +0530

    added: welcome to the linked

---
+++ src/com/bl/linked_list/Main.java
@@ -0,0 +1,9 @@
+package com.bl.linked_list;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to the Linked list");
+    }
+}
