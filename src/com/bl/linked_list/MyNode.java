a6ce9073eeff942d0938c1cd5856e8b80be2aeb9
Anicod <animeshraj735@gmail.com>
2022-06-13 10:23:21 +0530

    added: node is created in the linked list

---
+++ src/com/bl/linked_list/MyNode.java
@@ -0,0 +1,32 @@
+package com.bl.linked_list;
+
+public class MyNode {
+    private int value;
+    private MyNode next;
+
+    public MyNode(){
+        this.value = 0;
+        this.next = null;
+    }
+
+    public MyNode(int value, MyNode next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+}
